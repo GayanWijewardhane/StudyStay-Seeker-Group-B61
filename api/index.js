cf779f71b3e2349ae87f7281909de67f5ae841d9
GayanWijewardhane <gayanwijewardhanehmi@gmail.com>
2024-02-12 19:24:13 +0530

    Create pages and routes

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO)
+.then(() =>{
+    console.log('Successfully Conected to MongoDB!');
+   })
+   .catch((err) =>{
+    console.log(err);
+   });
 
 const app = express();
 app.listen(3000,()=> {console.log('Server Is Running On Port 3000 !!!!') ;
