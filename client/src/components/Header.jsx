87c5e44f6c4671ea9c4526471b3545943976e361
GayanWijewardhane <gayanwijewardhanehmi@gmail.com>
2024-02-14 00:18:12 +0530

    sign up page ui and functionalities

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
       <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-          <span className='text-slate-500'>StudyStay</span>
-          <span className='text-slate-700'> Seeker</span>
+          <span className='text-slate-500 a mr-1'>StudyStay</span>
+          <span className='text-slate-700 '> Seeker</span>
         </h1>
         </Link>
 
@@ -20,13 +20,13 @@ export default function Header() {
 
         <ul className='flex gap-4'>
         <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover underline'>Home</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
         </Link>
         <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover underline'>About</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
         </Link>
         <Link to='/sign-in'>
-            <li className='text-slate-700 hover underline'>Sign in</li>
+            <li className='text-slate-700  hover:underline'>Sign in</li>
             </Link>
         </ul>
       </div>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+
+
 
 export default function SignUp() {
-  return (
-    <div>SignUp</div>
-  )
+  const [formData,setFormData] = useState({});
+  const [error,setError] =useState(null);
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange =(e) =>
+{
+setFormData(
+  {
+   ...formData,
+   [e.target.id]: e.target.value,
+
+  });
+} ;
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    setLoading(true);
+  const res = await fetch('api/auth/signup',
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  });
+  const data = await res.json();
+  console.log(data);
+  if (data.success === false) {
+    setLoading(false);
+    setError(data.message);
+    return;
+  }
+  setLoading(false);
+  setError(null);
+  navigate('/sign-in');
+    
+  } catch (error) {
+    setLoading(false);
+    setError(error.message);
+  }
+  
+};
+
+   return (
+    <div className='p-3 max-w-md mx-auto  mt-20 bg-slate-200 rounded-lg shadow-md'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type='text' placeholder='Username' className='border rounded-lg px-4 py-3 focus:outline-none focus:border-blue-500' id='username' 
+        onChange={handleChange} />
+        <input type='text' placeholder='Email' className='border rounded-lg px-4 py-3 focus:outline-none focus:border-blue-500' id='email' 
+        onChange={handleChange} />
+        <input type='password' placeholder='Password' className='border rounded-lg px-4 py-3 focus:outline-none focus:border-blue-500' id='password' 
+        onChange={handleChange} />
+        
+        <button disabled={loading} className='bg-blue-500 text-white py-3 rounded-lg mt-3 hover:bg-blue-600 focus:outline-none'>{loading ? 'Loading..' : 'SIGN UP'}</button>
+      </form>
+
+      <div className='flex justify-center mt-6'>
+        <p className='text-gray-600'>Already have an account? </p>
+        <Link to="/sign-in" className='text-blue-500 hover:underline'>Sign in</Link>
+      </div>
+      {error && <p className='text-red-400 mt-5'>{error}</p>}
+    </div>
+  );
 }
