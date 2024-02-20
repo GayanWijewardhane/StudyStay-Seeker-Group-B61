ea932c55d60e55545223f9aae41792ee98fbefb3
GayanWijewardhane <gayanwijewardhanehmi@gmail.com>
2024-02-20 13:16:35 +0530

    user updating api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifytoken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test);
+router.post('/update/:id', verifytoken, updateUser);
 export default router;
