3c4f29d4fadc4fd36ba58da9d552d479da93c50f
GayanWijewardhane <gayanwijewardhanehmi@gmail.com>
2024-02-20 18:18:38 +0530

    add delete user function

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
 import { verifytoken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test);
 router.post('/update/:id', verifytoken, updateUser);
+router.delete('/delete/:id', verifytoken, deleteUser);
 export default router;
