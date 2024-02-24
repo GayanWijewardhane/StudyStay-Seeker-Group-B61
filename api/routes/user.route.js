8aa9864d29ef861e4957d46bfac76df110e19d5f
GayanWijewardhane <gayanwijewardhanehmi@gmail.com>
2024-02-24 19:33:17 +0530

    create show user listing api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser, getUserListings } from '../controllers/user.controller.js';
 import { verifytoken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,4 +7,5 @@ const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id', verifytoken, updateUser);
 router.delete('/delete/:id', verifytoken, deleteUser);
+router.get('/listings/:id', verifytoken, getUserListings);
 export default router;
