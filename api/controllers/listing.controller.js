56b6454ceb20b1ff6f46a487fa35b7a4b1839b27
GayanWijewardhane <gayanwijewardhanehmi@gmail.com>
2024-02-24 23:35:41 +0530

    delete listing function

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -9,3 +9,19 @@ export const createListing = async (req, res, next) => {
      next (error);
     }
 };
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(errorHandler(404, "Listing not found"));
+    }
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, "You can delete only your listings!"));
+    }
+    try {
+      await Listing.findByIdAndDelete(req.params.id);
+      res.status(200).json("Listing has been deleted");  
+    } catch (error) {
+        next(error);
+    }
+};
